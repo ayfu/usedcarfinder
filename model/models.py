278678044650bffb9472ff201ae09d1f00f469b1
Anthony Fu <anthnyfu@gmail.com>
2016-04-24 10:38:17 -0700

    add imports from jupyter notebook

--- model/models.py
+++ model/models.py
@@ -22,7 +22,7 @@ import pandas as pd
 import numpy as np
 import MySQLdb
 import matplotlib.pyplot as plt
-from sklearn.preprocessing import OneHotEncoder, LabelEncoder, MinMaxScaler
+from sklearn.preprocessing import OneHotEncoder, LabelEncoder, MinMaxScaler, scale
 from sklearn.cross_validation import train_test_split
 from sklearn.cross_validation import KFold
 from sklearn.ensemble import GradientBoostingRegressor, RandomForestRegressor
